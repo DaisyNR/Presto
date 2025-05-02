284fdab3c291f22862d5d7ba3e9b133de41e76a1
Daisy Nardella <daisy50@live.it>
2025-05-03 00:39:01 +0200

    aggiunto lato responsive

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let navbar = document.querySelector('#navbar');
-let icons = document.querySelectorAll('.icon');
+let logos = document.querySelectorAll('.logo');
 let links = document.querySelectorAll('.nav-link');
+let icons = document.querySelectorAll('.icon');
+let check = false;
 
 window.addEventListener('scroll', ()=>{
     let scrolled = window.scrollY;
@@ -8,24 +10,43 @@ window.addEventListener('scroll', ()=>{
     if(scrolled > 0){
         navbar.classList.remove('bg-green')
         navbar.classList.add('navCustom')
-        icons.forEach((icon)=>{
-            icon.style.color = 'var(--white)'
-            icon.classList.add('icon-custom')
+        logos.forEach((logo)=>{
+            logo.style.color = 'var(--white)'
+            logo.style.transform = 'rotate(180deg)'
         });
         links.forEach((link)=>{
-            link.classList.add('nav-link-custom')
+            link.classList.add('nav-linkCustom')
         });
     }else{
         navbar.classList.add('bg-green')
         navbar.classList.remove('navCustom')
-        icons.forEach((icon)=>{
-            icon.style.color = 'var(--red)'
-            icon.classList.remove('icon-custom')
+        logos.forEach((logo)=>{
+            logo.style.color = 'var(--red)'
+            logo.style.transform = ''
         });
         links.forEach((link)=>{
-            link.classList.remove('nav-link-custom')
+            link.classList.remove('nav-linkCustom')
         });
     }
 
 });
+
+
+icons.forEach((icon)=>{
+
+    if(check == false){
+        icon.addEventListener('click', ()=>{
+            icon.style.transform = 'rotate(-360deg)';
+            icon.style.color = 'var(--white)'
+            check = true;
+        });
+    }else{
+        check = false;
+    }
+
+
+});
+
+
+
 
